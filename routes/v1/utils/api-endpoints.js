e315c49733744e3d580821415ca249f9cb62f0f2
Aishwarya Sinhasane <avsinhas@iu.edu>
2023-08-03 14:29:23 -0400

    Changes for Seperate Queries folder aaded

--- routes/v1/utils/api-endpoints.js
+++ routes/v1/utils/api-endpoints.js
@@ -1,23 +1,15 @@
-
+import { readFileSync, writeFileSync } from 'fs';
 import { selectRemoteObjects } from './sparql.js';
 
+const sparqlEndpoint = 'https://lod.humanatlas.io/sparql';
 
 export async function getDatasetTechnologyNames() {
-  const sparqlEndpoint = 'https://lod.humanatlas.io/sparql';
 
-  const sparqlQuery = `
-      PREFIX ccf: <http://purl.org/ccf/>
-      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-  
-      SELECT DISTINCT ?allTech
-      WHERE {
-        ?subject ccf:technology ?allTech .
-      }
-    `;
+  const queryFilePath = 'routes/v1/queries/dataset-technology-names.rq';
+  const sparqlQuery = readFileSync(queryFilePath).toString();
 
   try {
     // Get results as an array of objects
-    console.log('Query Execution Started')
     const results = await selectRemoteObjects(sparqlQuery, sparqlEndpoint);
     console.log('Results as array of objects:');
     console.log(results);
@@ -29,19 +21,9 @@ export async function getDatasetTechnologyNames() {
 }
 
 export async function getTissueProviderNames() {
-  const sparqlEndpoint = 'https://lod.humanatlas.io/sparql';
 
-  const sparqlQuery = `
-      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-      PREFIX ccf: <http://purl.org/ccf/>
-      
-      SELECT DISTINCT ?providerName
-      WHERE {
-        ?tissueDonor a ccf:Donor ;
-                    ccf:tissue_provider_name ?providerName .
-      }    
-    `;
-
+  const queryFilePath = 'routes/v1/queries/tissue-provider-names.rq';
+  const sparqlQuery = readFileSync(queryFilePath).toString();
   try {
     // Get results as an array of objects
     const results = await selectRemoteObjects(sparqlQuery, sparqlEndpoint);
