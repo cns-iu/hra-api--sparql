d77ac0f02a67fdbf2206459289ff31ca5d74722a
Bruce W. Herr II <bherr@indiana.edu>
2023-08-22 11:03:14 -0400

    Added constructJsonLd

--- routes/v1/utils/sparql.js
+++ routes/v1/utils/sparql.js
@@ -1,4 +1,5 @@
 import { SparqlEndpointFetcher } from 'fetch-sparql-endpoint';
+import jsonld from 'jsonld';
 
 /**
  * Run a SPARQL query and return results as an array of values
@@ -43,4 +44,34 @@ export async function selectRemoteObjectsForOntology(query, sparqlEndpoint) {
       resolve(results);
     });
   });
+}
+
+/**
+ * Generator that constructs a JSON-LD object based on a SPARQL CONSTRUCT query
+ *
+ * @param {string} query the SPARQL query as a string
+ * @param {string} sparqlEndpoint the remote SPARQL endpoint to query
+ * @param {object | undefined} frameObj an optional frame object for json-ld
+ * @returns a JSON-LD object
+ */
+export async function constructJsonLd(query, sparqlEndpoint, frameObj = undefined) {
+  const fetcher = new SparqlEndpointFetcher({});
+  const stream = await fetcher.fetchTriples(sparqlEndpoint, query);
+  return new Promise((resolve, _reject) => {
+    const results = [];
+    stream.on('data', (quad) => {
+      results.push(quad);
+    });
+    stream.on('end', () => {
+      resolve(results);
+    });
+  })
+    .then((data) => jsonld.fromRDF(data))
+    .then((data) => {
+      if (frameObj) {
+        return jsonld.frame(data, frameObj);
+      } else {
+        return data;
+      }
+    });
 }
